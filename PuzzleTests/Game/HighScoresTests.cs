1b2acba748922ac8b17e0e62166f3d25154d8ba8
monkog <monkog@wp.pl>
2019-06-23 18:01:27 +0200

    Added missing high score tests

--- PuzzleTests/Game/HighScoresTests.cs
+++ PuzzleTests/Game/HighScoresTests.cs
@@ -6,6 +6,14 @@ namespace PuzzleTests.Game
 	[TestClass]
 	public class HighScoresTests
 	{
+		private HighScores _unitUnderTest;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_unitUnderTest = new HighScores();
+		}
+
 		[TestMethod]
 		public void Ctor_ValidParameters_PropertiesInitialized()
 		{
@@ -14,6 +22,42 @@ namespace PuzzleTests.Game
 			Assert.IsNotNull(unitUnderTest.EasyHighScores);
 			Assert.IsNotNull(unitUnderTest.MediumHighScores);
 			Assert.IsNotNull(unitUnderTest.HardHighScores);
+		}
+
+		[TestMethod]
+		public void AddHighScore_Easy_AddedToEasyList()
+		{
+			var highScore = new HighScoreRecord("Alex", 4, 12);
+
+			_unitUnderTest.Add(Difficulty.Easy, highScore);
+
+			CollectionAssert.Contains(_unitUnderTest.EasyHighScores, highScore);
+			CollectionAssert.DoesNotContain(_unitUnderTest.MediumHighScores, highScore);
+			CollectionAssert.DoesNotContain(_unitUnderTest.HardHighScores, highScore);
+		}
+
+		[TestMethod]
+		public void AddHighScore_Medium_AddedToMediumList()
+		{
+			var highScore = new HighScoreRecord("Alex", 4, 12);
+
+			_unitUnderTest.Add(Difficulty.Medium, highScore);
+
+			CollectionAssert.DoesNotContain(_unitUnderTest.EasyHighScores, highScore);
+			CollectionAssert.Contains(_unitUnderTest.MediumHighScores, highScore);
+			CollectionAssert.DoesNotContain(_unitUnderTest.HardHighScores, highScore);
+		}
+
+		[TestMethod]
+		public void AddHighScore_Hard_AddedToMediumList()
+		{
+			var highScore = new HighScoreRecord("Alex", 4, 12);
+
+			_unitUnderTest.Add(Difficulty.Hard, highScore);
+
+			CollectionAssert.DoesNotContain(_unitUnderTest.EasyHighScores, highScore);
+			CollectionAssert.DoesNotContain(_unitUnderTest.MediumHighScores, highScore);
+			CollectionAssert.Contains(_unitUnderTest.HardHighScores, highScore);
 		}
 	}
 }
